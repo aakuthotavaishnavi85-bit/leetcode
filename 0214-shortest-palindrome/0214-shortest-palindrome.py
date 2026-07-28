062dd1672273f417628cc92059ffdd2437c40565
aakuthotavaishnavi85-bit <aakuthotavaishnavi85@gmail.com>
2026-07-28 19:07:07 +0530

    Time: 1223 ms (9.93%), Space: 19.9 MB (63.66%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,14 +1,15 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         n=len(s)
-        if n>0:
+        if n==0:
+            return ""
+        else:
             for i in range(n,0,-1):
                 prefix=s[:i]
                 if prefix==prefix[::-1]:
                     break
             remaining=s[i:]
             return remaining[::-1]+s
-        else:
-            return ""
+        
 
         
