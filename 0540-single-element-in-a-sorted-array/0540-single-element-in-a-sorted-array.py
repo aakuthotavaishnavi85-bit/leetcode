b6eccee607d6af978c0c6309c9df2a7f988f2f44
aakuthotavaishnavi85-bit <aakuthotavaishnavi85@gmail.com>
2026-07-29 10:19:11 +0530

    Time: 0 ms (100%), Space: 26.9 MB (61.64%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
+        """for i in range(len(nums)):
             if len(nums)==1:
                 return nums[i]
             if i==0:
@@ -14,4 +14,18 @@ class Solution:
             else:
                 if nums[i]==nums[i+1] or nums[i]==nums[i-1]:
                     continue
-                return nums[i]
+                return nums[i]"""
+        left=0
+        right=len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]==nums[mid^1]:
+                left=mid+1
+            else:
+                right=mid
+        return nums[left]
+            
+
+            
+            
+            
