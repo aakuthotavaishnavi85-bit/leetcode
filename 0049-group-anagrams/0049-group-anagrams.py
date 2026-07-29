07e0af1376df39931fd0ae53c837d8b3c09247ac
aakuthotavaishnavi85-bit <aakuthotavaishnavi85@gmail.com>
2026-07-29 12:22:10 +0530

    Time: 12 ms (56.43%), Space: 22 MB (79.06%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        n=len(strs)
+        group={}
+        for i in strs:
+            k=" ".join(sorted(i))
+            if k not in group:
+                group[k]=[ ]
+            group[k].append(i)
+        return list(group.values())
+
